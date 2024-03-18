15a6c2f9026bab96c5eb17c001c83959ff6649cb
Kippyi <138270550+Kippyi@users.noreply.github.com>
2024-03-18 23:41:59 +1000

    Update config.lua
    Added in auto detect * more to come soon *

--- chroma-toggleableNPC/config.lua
+++ chroma-toggleableNPC/config.lua
@@ -9,18 +9,13 @@ Config = {
     end,
 }
 
-Config.Notification = 'auto_detect' --[ 'auto_detect' / 'other' ]   If you select 'auto_detect', only okokNotify, ps-ui and ox_lib notifications will be detected. Use 'other' for custom notification resources.
+Config.Notification = 'okokNotify' --[ 'auto_detect' / 'other' ]   If you select 'auto_detect', only okokNotify notifications will be detected. Use 'other' for custom notification resources.
 -- Auto Detect
 
 if Config.Notification == 'auto_detect' then
     if GetResourceState('okokNotify') == 'started' then
         Config.Notification = 'okokNotify'
-    elseif GetResourceState('ps-ui') == 'started' then
-        Config.Notification = 'ps-ui'
-    elseif GetResourceState('ox_lib') == 'started' then
-        Config.Notification = 'ox_lib'
     else
-            Config.Notification = 'chat'
-        end
+        Config.Notification = 'chat'
     end
 end
