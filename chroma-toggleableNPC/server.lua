b541de4d06c3ea0892a2b29f2915d7bf203c27f1
Kippyi <138270550+Kippyi@users.noreply.github.com>
2024-03-18 23:06:43 +1000

    Update server.lua

--- chroma-toggleableNPC/server.lua
+++ chroma-toggleableNPC/server.lua
@@ -1,6 +1,42 @@
-local Enabled = Config.EnabledOnStartup
-RegisterCommand("togglenpc", function(source, args, rawCommand)
+local Config = {
+    EnabledOnStartup = false, -- whether NPCs are enabled on server start
+    HasPermission = function(src)
+        return IsPlayerAceAllowed(src, "toggle-npc")
+    end,
+    Notify = function(src, msg, type)
+        if Config.Notification == 'auto_detect' then
+            if GetResourceState('okokNotify') == 'started' then
+                exports['okokNotify']:Alert('SYSTEM', msg, 5000, 'success')
+            elseif GetResourceState('ps-ui') == 'started' then
+                exports['ps-ui']:Notify(msg, 'success', 5000)
+            elseif GetResourceState('ox_lib') == 'started' then
+                lib.notify({
+                    title = 'SYSTEM',
+                    description = msg,
+                    type = 'success'
+                })
+            else
+                TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
+            end
+        elseif Config.Notification == 'okokNotify' then
+            exports['okokNotify']:Alert('SYSTEM', msg, 5000, 'success')
+        elseif Config.Notification == 'ps-ui' then
+            exports['ps-ui']:Notify(msg, 'success', 5000)
+        elseif Config.Notification == 'ox_lib' then
+            lib.notify({
+                title = 'SYSTEM',
+                description = msg,
+                type = 'success'
+            })
+        else
+            TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
+        end
+    end,
+}
 
+local Enabled = Config.EnabledOnStartup
+
+RegisterCommand("togglenpc", function(source, args, rawCommand)
     if source == 0 then
         Enabled = not Enabled
         SetRoutingBucketPopulationEnabled(0, Enabled)
