fe18f14b8077cce4b6f55eefa34ec66dcce4fa8f
Kippyi <138270550+Kippyi@users.noreply.github.com>
2024-03-18 23:41:39 +1000

    Update server.lua
    added in okokNotify Auto Detect

--- chroma-toggleableNPC/server.lua
+++ chroma-toggleableNPC/server.lua
@@ -1,38 +1,30 @@
-local Config = {
+Config = {
     EnabledOnStartup = false, -- whether NPCs are enabled on server start
     HasPermission = function(src)
         return IsPlayerAceAllowed(src, "toggle-npc")
     end,
-    Notify = function(src, msg, type)
-        if Config.Notification == 'auto_detect' then
-            if GetResourceState('okokNotify') == 'started' then
-                exports['okokNotify']:Alert('SYSTEM', msg, 5000, 'success')
-            elseif GetResourceState('ps-ui') == 'started' then
-                exports['ps-ui']:Notify(msg, 'success', 5000)
-            elseif GetResourceState('ox_lib') == 'started' then
-                lib.notify({
-                    title = 'SYSTEM',
-                    description = msg,
-                    type = 'success'
-                })
-            else
-                TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
-            end
-        elseif Config.Notification == 'okokNotify' then
-            exports['okokNotify']:Alert('SYSTEM', msg, 5000, 'success')
-        elseif Config.Notification == 'ps-ui' then
-            exports['ps-ui']:Notify(msg, 'success', 5000)
-        elseif Config.Notification == 'ox_lib' then
-            lib.notify({
-                title = 'SYSTEM',
-                description = msg,
-                type = 'success'
-            })
-        else
-            TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
-        end
-    end,
+    Notification = Config.Notification, -- Change this to the desired notification method
 }
+
+-- Auto Detect for Notification
+if Config.Notification == 'auto_detect' then
+    if GetResourceState('okokNotify') == 'started' then
+        Config.Notification = 'okokNotify'
+    else
+        Config.Notification = 'chat'
+    end
+end
+
+
+
+local function Notify(src, msg, type)
+    if Config.Notification == 'okokNotify' then
+        TriggerClientEvent('okokNotify:Alert', src, 'SYSTEM', msg, 5000, 'success', true) -- Assuming playSound is set to true
+    else
+        -- Default to chat message if notification method is not recognized
+        TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
+    end
+end
 
 local Enabled = Config.EnabledOnStartup
 
@@ -46,9 +38,9 @@ RegisterCommand("togglenpc", function(source, args, rawCommand)
     if Config.HasPermission(source) then
         Enabled = not Enabled
         SetRoutingBucketPopulationEnabled(0, Enabled)
-        Config.Notify(source, string.format("NPCs have been %s", Enabled and "enabled!" or "disabled!"), 'success')
+        Notify(source, string.format("NPCs have been %s", Enabled and "enabled!" or "disabled!"), 'success')
     else
-        Config.Notify(source, "You do not have permission to use this command.", 'error')
+        Notify(source, "You do not have permission to use this command.", 'error')
     end
 end, false)
 
@@ -56,3 +48,4 @@ CreateThread(function()
     SetRoutingBucketPopulationEnabled(0, Enabled)
     print(string.format("^6[chroma-toggleableNPC] ^7NPCs have been: %s", Enabled and "^2enabled.^7" or "^1disabled.^7"))
 end)
+
