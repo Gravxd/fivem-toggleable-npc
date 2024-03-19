de4bf72c1e6068667c74c41d24c6ed4cdd6e3760
Kip <138270550+Kippyi@users.noreply.github.com>
2024-03-19 16:42:30 +1000

    Update server.lua
    Added in mythic_notify

--- chroma-toggleableNPC/server.lua
+++ chroma-toggleableNPC/server.lua
@@ -10,6 +10,8 @@ Config = {
 if Config.Notification == 'auto_detect' then
     if GetResourceState('okokNotify') == 'started' then
         Config.Notification = 'okokNotify'
+    elseif GetResourceState('mythic_notify') == 'started' then
+        Config.Notification = 'mythic_notify'
     else
         Config.Notification = 'chat'
     end
@@ -20,11 +22,14 @@ end
 local function Notify(src, msg, type)
     if Config.Notification == 'okokNotify' then
         TriggerClientEvent('okokNotify:Alert', src, 'SYSTEM', msg, 5000, 'success', true) -- Assuming playSound is set to true
+    elseif Config.Notification == 'mythic_notify' then
+        TriggerClientEvent('mythic_notify:client:SendAlert', src, { type = 'success', text = msg , style = { ['background-color'] = '#ffffff', ['color'] = '#000000' } }) -- Change #color to what ever you want https://imagecolorpicker.com/color-code
     else
         -- Default to chat message if notification method is not recognized
         TriggerClientEvent("chatMessage", src, "SYSTEM", {255, 0, 0}, msg)
     end
 end
+
 
 local Enabled = Config.EnabledOnStartup
 
