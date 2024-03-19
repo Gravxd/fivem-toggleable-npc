e208ec9e5d597118f9d9af33d95e4d500862173e
Kip <138270550+Kippyi@users.noreply.github.com>
2024-03-19 16:42:09 +1000

    Update config.lua
    Added in mythic_notify

--- chroma-toggleableNPC/config.lua
+++ chroma-toggleableNPC/config.lua
@@ -9,12 +9,15 @@ Config = {
     end,
 }
 
-Config.Notification = 'okokNotify' --[ 'auto_detect' / 'other' ]   If you select 'auto_detect', only okokNotify notifications will be detected. Use 'other' for custom notification resources.
+Config.Notification = 'auto_detect' --[ 'auto_detect' / 'other' ]   If you select 'auto_detect', only okokNotify, mythic_notify, notifications will be detected. Use 'other' for custom notification resources.
 -- Auto Detect
 
 if Config.Notification == 'auto_detect' then
     if GetResourceState('okokNotify') == 'started' then
         Config.Notification = 'okokNotify'
+    elseif GetResourceState('mythic_notify') == 'started' then
+        Config.Notification = 'mythic_notify'
+
     else
         Config.Notification = 'chat'
     end
